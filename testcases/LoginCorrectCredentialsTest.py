66f8d8c7f6e95cf787478d473cd0ae0253642d69
anjalysuresh <anjalysuresh@yahoo.co.in>
2018-06-21 15:49:30 +0530

    Update LoginCorrectCredentialsTest.py

--- testcases/LoginCorrectCredentialsTest.py
+++ testcases/LoginCorrectCredentialsTest.py
@@ -2,16 +2,19 @@ __author__ = 'urmi and minali'
 
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
 
 class LoginCorrect(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        #self.driver = webdriver.Firefox()
+        self.driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub', desired_capabilities=DesiredCapabilities.FIREFOX) 
 
     def test_LoginCorrect(self):
         user ="testuser"
         pwd= "collaborative"
-        driver = webdriver.Firefox()
+        #driver = webdriver.Firefox()
         driver.get("http://10.129.26.119//login")
         elem = driver.find_element_by_id("id_username")
         elem.send_keys(user)
