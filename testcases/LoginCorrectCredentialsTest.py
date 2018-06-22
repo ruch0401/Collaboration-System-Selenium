9bb86a96e03cab43d64f798051d014abeb217cd1
anjalysuresh <anjalysuresh@yahoo.co.in>
2018-06-22 12:12:02 +0530

    Update LoginCorrectCredentialsTest.py

--- testcases/LoginCorrectCredentialsTest.py
+++ testcases/LoginCorrectCredentialsTest.py
@@ -6,15 +6,16 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 class LoginCorrect(unittest.TestCase):
-    driver = webdriver.Remote(command_executor='http://10.129.132.104:4444/wd/hub', desired_capabilities=DesiredCapabilities.FIREFOX) 
-   # def setUp(self):
+    
+    def setUp(self):
+        self.driver = webdriver.Remote(command_executor='http://10.129.132.104:4444/wd/hub', desired_capabilities=DesiredCapabilities.FIREFOX) 
         #self.driver = webdriver.Firefox()
         
 
     def test_LoginCorrect(self):
         user ="testuser"
         pwd= "collaborative"
-        #driver = webdriver.Remote(command_executor='http://10.129.132.104:4444/wd/hub', desired_capabilities=DesiredCapabilities.FIREFOX) 
+        driver = webdriver.Remote(command_executor='http://10.129.132.104:4444/wd/hub', desired_capabilities=DesiredCapabilities.FIREFOX) 
 
         #driver = webdriver.Firefox()
         driver.get("http://10.129.26.119//login")
